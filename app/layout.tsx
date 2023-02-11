f7bd4b1d786d89836273baaa550224ad5f3d0710
Vojaswwin <vojaswwin.ap@thoughtworks.com>
2023-02-11 13:12:13 +0530

    Notes making app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import Link from 'next/link';
 
 export default function RootLayout({
   children,
@@ -12,7 +13,19 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <main>
+          <nav>
+            <Link href="/">
+              Home
+            </Link>
+            <Link href="/notes">
+              Notes
+            </Link>
+          </nav>
+          {children}
+        </main>
+        </body>
     </html>
   )
 }
